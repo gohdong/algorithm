266de03b0cafb7ecd9290877761eae6e9e176f9a
gohdong <aldehf420@naver.com>
2021-08-26 15:50:05 +0900

    factorization

---
+++ my_library/factorization.py
@@ -0,0 +1,17 @@
+def factorization(a: int):
+	result = {}
+	
+	prime = 2
+	while a != 1:
+		if a % prime == 0:
+			if prime not in result:
+				result[prime] = 1
+			else:
+				result[prime] +=1
+			a /= prime
+		else:
+			prime +=1
+
+	return(result)
+
+print(factorization(1312))
