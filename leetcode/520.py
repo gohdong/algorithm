63b500b4e452b220429c817b1235242018446951
gohdong <aldehf420@naver.com>
2021-08-31 07:42:37 +0900

    2021-08-31 07:42

---
+++ leetcode/520.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class ListNode:
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
+
+class Solution:
+	def detectCapitalUse(self, word: str) -> bool:
+		return word == word.upper() or word == word.lower or word == word[0].upper()+word[1:].lower()
+        
+
+
+
+solution = Solution()
+print(solution.detectCapitalUse(word = "USA"))
