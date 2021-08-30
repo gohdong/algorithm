934f411eb7c593920d9ea2733ec8918a7abcba8d
gohdong <aldehf420@naver.com>
2021-08-30 21:48:08 +0900

    2021-08-30 21:48

---
+++ leetcode/409.py
@@ -0,0 +1,22 @@
+from typing import Counter, List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        answer = 0
+        counter_s = Counter(s)
+        max_odd = 0
+        for c in counter_s:
+            if counter_s[c] % 2:
+                max_odd=1
+            answer += counter_s[c] - counter_s[c] % 2
+        return answer + max_odd
+
+solution = Solution()
+print(solution.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
