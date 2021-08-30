541949f3e8941a0b6ab21ed873eac4437baeba13
gohdong <aldehf420@naver.com>
2021-08-30 22:20:49 +0900

    2021-08-30 22:20

---
+++ leetcode/459.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class ListNode:
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
+
+class Solution:
+	def repeatedSubstringPattern(self, s: str) -> bool:
+		for i in range(2,len(s)):
+			if len(s) % i == 0 and not s.replace(s[:i],""):
+				return True
+				
+		return False
+        
+
+
+
+solution = Solution()
+print(solution.repeatedSubstringPattern(s = "abab"))
