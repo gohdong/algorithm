17afab1a0b9b22796f6399169a14029bde660d1e
gohdong <aldehf420@naver.com>
2021-08-31 12:39:47 +0900

    2021-08-31 12:39

---
+++ leetcode/599.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class ListNode:
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
+
+
+class Solution:
+	def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+		answer = []
+		same_set = set(list1).intersection(set(list2))
+		same_dict = {x:i+list2.index(x) for i,x in enumerate(list1) if x in same_set}
+		temp = sorted(same_dict,key= lambda x : same_dict[x])
+		for s in temp:
+			if same_dict[s] == same_dict[temp[0]]:
+				answer.append(s)
+		# list2_dict = {x:i for i,x in enumerate(list2)}
+
+
+
+		return answer
+
+
+solution = Solution()
+print(solution.findRestaurant(list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Shogun","Burger King"]))
