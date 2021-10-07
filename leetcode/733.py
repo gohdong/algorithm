1878d92e09ea0b81179204e18ea879c7b114bce0
gohdong <aldehf420@naver.com>
2021-10-07 16:38:31 +0900

    dfs to iter

--- leetcode/733.py
+++ leetcode/733.py
@@ -8,6 +8,25 @@ class ListNode:
 
 
 class Solution:
+    def floodFill2(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        start_color = image[sr][sc]
+        if start_color == newColor:
+            return image
+        stack = [(sr,sc)]
+
+        while stack:
+            temp = stack.pop()
+            image[temp[0]][temp[1]] = newColor
+            if temp[0] - 1 >= 0 and image[temp[0]-1][temp[1]] == start_color:
+                stack.append((temp[0]-1,temp[1]))
+            if temp[1] - 1 >= 0 and image[temp[0]][temp[1]-1] == start_color:
+                stack.append((temp[0],temp[1]-1))
+            if temp[0] + 1 < len(image) and image[temp[0]+1][temp[1]] == start_color:
+                stack.append((temp[0]+1,temp[1]))
+            if temp[1] + 1 < len(image[0]) and image[temp[0]][temp[1]+1] == start_color:
+                stack.append((temp[0],temp[1]+1))
+        return image
+
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         start_color = image[sr][sc]
         if start_color == newColor:
